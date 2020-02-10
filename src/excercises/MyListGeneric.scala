47c8d4685f6a192008f2f86cafc3b1e5166ac1e2
Mohan Chafekar <mohanchafekar@gmail.com>
2020-02-10 22:32:08 +0530

    used case class and case object

--- src/excercises/MyListGeneric.scala
+++ src/excercises/MyListGeneric.scala
@@ -28,7 +28,7 @@ abstract class MyList[+A] {
 // Implementing empty list and non empty list
 
 //Empty List
-object Empty extends MyList[Nothing]{
+case object Empty extends MyList[Nothing]{
   def head: Nothing = throw new NoSuchElementException
   def tail: MyList[Nothing] = throw new NoSuchElementException
   def isEmpty: Boolean = true
@@ -44,7 +44,7 @@ object Empty extends MyList[Nothing]{
 }
 
 // Non empty list
-class Cons[+A](h: A, t: MyList[A]) extends MyList[A]{
+case class Cons[+A](h: A, t: MyList[A]) extends MyList[A]{
 
   def head: A = h
   def tail: MyList[A] = t
@@ -104,6 +104,7 @@ object ListTest extends App{
   //val listOfIntegers: MyList[Int] = Empty
   //val listOfStrings: MyList[String] = Empty
   val listOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  val cloneListOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
   val anotherListOfIntegers: MyList[Int] = new Cons(1, new Cons(4, new Cons(5, Empty)))
   val listOfStrings: MyList[String] = new Cons("Hello", new Cons("SCALA", Empty))
 
@@ -123,4 +124,6 @@ object ListTest extends App{
   println(listOfIntegers.flatMap(new MyTransformer[Int, MyList[Int]] {
     override def transform(elem: Int): MyList[Int] = new Cons(elem, new Cons[Int](elem+1, Empty))
   }).toString)
+
+  println(listOfIntegers == cloneListOfIntegers) // This is true because of case keyword
 }
